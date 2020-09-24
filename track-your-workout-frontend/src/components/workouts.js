10a1959caa86dcd146e11616612d361ff1126afa
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-24 14:51:29 -0400

    updates render and  fetchAndLoadWorkouts functions  to get join table info

--- track-your-workout-frontend/src/components/workouts.js
+++ track-your-workout-frontend/src/components/workouts.js
@@ -8,23 +8,38 @@ class Workouts{
     
     initBindingsAndEventListeners(){
         this.workoutContainer = document.getElementById('workout-container')
+        this.newWorkoutNameInput = document.getElementById("workout-name")
+        this.newWorkoutUrlInput = document.getElementById("workout-url")
+        this.newWorkoutTimeInput = document.getElementById("workout-time")
+        this.newWorkoutCategoryInput = document.getElementById("workout-category")
         this.workoutForm = document.getElementById('new-workout-form')
+        this.workoutForm.addEventListener("submit", this.createWorkout.bind(this))
     }
 
+    createWorkout(e){
+        e.preventDefault()
+        const name = this.newWorkoutNameInput.value
+        const url = this.newWorkoutUrlInput.value
+        const time = this.newWorkoutTimeInput.value
+        const category = this.newWorkoutCategoryInput.value
+        this.adapter.createWorkout(name, url, time, category).then(workout => {
+            console.log(workout)
+        })
+    }
 
     fetchAndLoadWorkouts(){
         this.adapter.getWorkouts().then(workouts =>{
-            workouts.forEach(workout => this.workouts.push(new Workout(workout)))
-            // return console.log(workout)
-        }).then(() =>{
+            // workouts["data"].forEach(element => console.log(element["attributes"]["date"]));
+            workouts["data"].forEach(workout => this.workouts.push(new Workout(workout)))
+        })
+        .then(() =>{
             this.render()
         })
     }
 
     render(){
-        // const workoutContainer = document.getElementById("workout-container")
+        console.log(this.workouts)
         const workoutArray = this.workouts.map(workout => workout.renderHTML()).join(' ')
-        console.log(this.workoutContainer)
         this.workoutContainer.innerHTML = `${workoutArray}`
     }
 }
