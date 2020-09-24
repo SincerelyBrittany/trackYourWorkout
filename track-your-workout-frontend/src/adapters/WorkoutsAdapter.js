677d8b98a0589fd233689a05a0b7817fd90f162d
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-24 14:49:46 -0400

    changes base url to join table

--- track-your-workout-frontend/src/adapters/WorkoutsAdapter.js
+++ track-your-workout-frontend/src/adapters/WorkoutsAdapter.js
@@ -2,11 +2,29 @@
 class WorkoutsAdapter {
     constructor(){
         this.baseURL = 
-        "http://localhost:3000/api/v1/workouts"
+        // "http://localhost:3000/api/v1/workouts"
+        "http://localhost:3000/api/v1/user_workouts"
     }
 
     getWorkouts(){
         return fetch(this.baseURL).then(res => res.json()
         )
+    }
+
+    createWorkout(name, url, time, category){
+        const data ={
+            name: name,
+            url: url,
+            time: time,
+            category_id: category
+        }
+        return fetch(this.baseURL, {
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({workout: data})
+        })
+        .then(res => res.json())
     }
 }
