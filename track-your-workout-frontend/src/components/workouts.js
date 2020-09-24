ab54a734c25ee9a3dd90ee8e4469f5758297ea3c
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-24 11:16:00 -0400

    refactors the render function

--- track-your-workout-frontend/src/components/workouts.js
+++ track-your-workout-frontend/src/components/workouts.js
@@ -2,9 +2,16 @@ class Workouts{
     constructor(){
         this.workouts = []
         this.adapter = new WorkoutsAdapter()
-        // this.bindEventListeners()
+        this.initBindingsAndEventListeners()
         this.fetchAndLoadWorkouts()
     }
+    
+    initBindingsAndEventListeners(){
+        this.workoutContainer = document.getElementById('workout-container')
+        this.workoutForm = document.getElementById('new-workout-form')
+    }
+
+
     fetchAndLoadWorkouts(){
         this.adapter.getWorkouts().then(workouts =>{
             workouts.forEach(workout => this.workouts.push(new Workout(workout)))
@@ -15,17 +22,9 @@ class Workouts{
     }
 
     render(){
-        console.log(this.workouts)
-        const workoutContainer = document.getElementById("workout-container")
-        // workoutContainer.dataset.id = this.workout.id
-        const workoutArray = this.workouts.map(workout => `
-        <div class="${workout.id}"data-set-id="${workout.id}">
-        <h3>${workout.name}</h3>
-        <iframe width="420" height="315" src="${workout.url}/embed" frameborder="0" allowfullscreen></iframe>
-        <p> ${workout.time} </p>
-        <p> ${workout.category} </p>
-        </div>`).join(' ')
-        console.log(workoutArray)
-        workoutContainer.innerHTML = `${workoutArray}`
+        // const workoutContainer = document.getElementById("workout-container")
+        const workoutArray = this.workouts.map(workout => workout.renderHTML()).join(' ')
+        console.log(this.workoutContainer)
+        this.workoutContainer.innerHTML = `${workoutArray}`
     }
 }
