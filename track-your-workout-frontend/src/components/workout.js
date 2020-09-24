2bff369ed6c25cabf555b5bf2cd647c45d1ad7ad
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-24 14:50:21 -0400

    updates the constructor and renderHTML function

--- track-your-workout-frontend/src/components/workout.js
+++ track-your-workout-frontend/src/components/workout.js
@@ -1,19 +1,21 @@
 class Workout{
     constructor(workoutJson){
         this.id = workoutJson.id
-        this.name = workoutJson.name
-        this.url = workoutJson.url
-        this.time = workoutJson.time
-        this.category = workoutJson.category_id
+        this.date = workoutJson.attributes.date
+        this.user = workoutJson.attributes.user
+        this.workout = workoutJson.attributes.workout
+        // this.time = workoutJson.time
+        // this.category = workoutJson.category_id
     }
 
     renderHTML(){
         return `
         <div class="${this.id}"data-set-id="${this.id}">
-        <h3>${this.name}</h3>
-        <iframe width="420" height="315" src="${this.url}/embed" frameborder="0" allowfullscreen></iframe>
-        <p> ${this.time} </p>
-        <p> ${this.category} </p>
+        <p> ${this.user.name} </p>
+        <h3>${this.workout.name}</h3>
+        <iframe width="420" height="315" src="${this.workout.url}/embed" frameborder="0" allowfullscreen></iframe>
+        <p> ${this.workout.time} </p>
+        <p> ${this.date} </p>
         </div>`
     }
 }
