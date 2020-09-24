6239e13d2511b84938d923b3435d59dbb2ae849f
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-24 11:15:33 -0400

    creates render html function to render each workout

--- track-your-workout-frontend/src/components/workout.js
+++ track-your-workout-frontend/src/components/workout.js
@@ -6,4 +6,14 @@ class Workout{
         this.time = workoutJson.time
         this.category = workoutJson.category_id
     }
+
+    renderHTML(){
+        return `
+        <div class="${this.id}"data-set-id="${this.id}">
+        <h3>${this.name}</h3>
+        <iframe width="420" height="315" src="${this.url}/embed" frameborder="0" allowfullscreen></iframe>
+        <p> ${this.time} </p>
+        <p> ${this.category} </p>
+        </div>`
+    }
 }
