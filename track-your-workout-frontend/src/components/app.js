4b01e7ebbd8efbd3a270cfe003e0a7898739aea4
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-26 15:23:52 -0400

    notes on next steps/functionality

--- track-your-workout-frontend/src/components/app.js
+++ track-your-workout-frontend/src/components/app.js
@@ -3,3 +3,15 @@ class App{
         this.workouts = new Workouts()
     }
 }
+
+//Functionality that you want:
+// CRUD a workout 
+// You want the workouts with the same date to render together in the same div
+// Set up users 
+// If date passes you want to remove the div 
+// User can only make workouts for 4 weeks in advance
+// Update date in serializer 
+// CRUD a workout 
+
+//LATER - Categories 
+//LATER - Incorparate youtube search functionality
