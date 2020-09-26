60881fed9646de95f4764124fe1975405aa2add7
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-26 14:54:40 -0400

    adds workout time

--- track-your-workout-frontend/src/components/workout.js
+++ track-your-workout-frontend/src/components/workout.js
@@ -13,7 +13,7 @@ class Workout{
                 <p> The user is: ${this.user} </p>
                 <h3> The workout name is: ${this.workout.name}</h3>
                 <iframe width="420" height="315" src="${this.workout.url}/embed" frameborder="0" allowfullscreen></iframe>
-                <p> The time is ${this.workout.time} </p>
+                <p> The time is ${this.workout.time_updated} </p>
         </div>`
     }   
 }
