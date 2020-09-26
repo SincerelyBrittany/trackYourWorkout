fc2ba7ff7bc56f651cc42aa221948c8afe45a308
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-25 21:54:10 -0400

    allows collapse for each div

--- track-your-workout-frontend/src/components/workout.js
+++ track-your-workout-frontend/src/components/workout.js
@@ -1,21 +1,42 @@
 class Workout{
     constructor(workoutJson){
         this.id = workoutJson.id
-        this.date = workoutJson.attributes.date
-        this.user = workoutJson.attributes.user
-        this.workout = workoutJson.attributes.workout
+        this.date = workoutJson.date
+        this.user = workoutJson.user.name
+        this.workout = workoutJson.workout
         // this.time = workoutJson.time
         // this.category = workoutJson.category_id
     }
 
+ 
+
     renderHTML(){
+        // const btn = document.createElement('button')
+        // btn.className = 'collapsible'
+        // btn.addEventListener("click", function() {
+        //     this.classList.toggle("active");
+        //     var content = this.nextElementSibling;
+        //     if (content.style.display === "block") {
+        //     content.style.display = "none";
+        //     } else {
+        //     content.style.display = "block";
+        //     }
+        // })
+        // const div = document.createElement('div')
+        // div.dataset.id = this.id
+        // div.className = this.workout.name
+        // const p = document.createElement('p')
+        // p.innerText = `The user is: ${this.user}`
+        // btn.append(div,p)
         return `
-        <div class="${this.id}"data-set-id="${this.id}">
-        <p> ${this.user.name} </p>
-        <h3>${this.workout.name}</h3>
-        <iframe width="420" height="315" src="${this.workout.url}/embed" frameborder="0" allowfullscreen></iframe>
-        <p> ${this.workout.time} </p>
-        <p> ${this.date} </p>
+        <button type="button" class="collapsible">${this.date}</button>
+            <div class="${this.id} content"data-set-id="${this.id}">
+                <p> The user is: ${this.user} </p>
+                <h3> The workout name is: ${this.workout.name}</h3>
+                <iframe width="420" height="315" src="${this.workout.url}/embed" frameborder="0" allowfullscreen></iframe>
+                <p> The time is ${this.workout.time} </p>
         </div>`
     }
+
+   
 }
--- track-your-workout-frontend/src/components/workouts.js
+++ track-your-workout-frontend/src/components/workouts.js
@@ -4,6 +4,7 @@ class Workouts{
         this.adapter = new WorkoutsAdapter()
         this.initBindingsAndEventListeners()
         this.fetchAndLoadWorkouts()
+        
     }
     
     initBindingsAndEventListeners(){
@@ -11,6 +12,7 @@ class Workouts{
         this.newWorkoutNameInput = document.getElementById("workout-name")
         this.newWorkoutUrlInput = document.getElementById("workout-url")
         this.newWorkoutTimeInput = document.getElementById("workout-time")
+        this.coll = document.getElementsByClassName("collapsible");
         this.newWorkoutCategoryInput = document.getElementById("workout-category")
         this.workoutForm = document.getElementById('new-workout-form')
         this.workoutForm.addEventListener("submit", this.createWorkout.bind(this))
@@ -18,6 +20,7 @@ class Workouts{
 
     createWorkout(e){
         e.preventDefault()
+        // const { name, url, etc} = this.newWorkoutNameInput
         const name = this.newWorkoutNameInput.value
         const url = this.newWorkoutUrlInput.value
         const time = this.newWorkoutTimeInput.value
@@ -30,7 +33,7 @@ class Workouts{
     fetchAndLoadWorkouts(){
         this.adapter.getWorkouts().then(workouts =>{
             // workouts["data"].forEach(element => console.log(element["attributes"]["date"]));
-            workouts["data"].forEach(workout => this.workouts.push(new Workout(workout)))
+            workouts.forEach(workout => this.workouts.push(new Workout(workout)))
         })
         .then(() =>{
             this.render()
@@ -38,8 +41,27 @@ class Workouts{
     }
 
     render(){
-        console.log(this.workouts)
+        // console.log(this.workouts)
         const workoutArray = this.workouts.map(workout => workout.renderHTML()).join(' ')
         this.workoutContainer.innerHTML = `${workoutArray}`
+        this.coll.addEventListener("click", this.collapseFunc())
+        // console.log(this.workoutContainer, "this is workout container")
+    }
+
+    collapseFunc(){
+        // var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < this.coll.length; i++) {
+            this.coll[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                content.style.display = "none";
+                } else {
+                content.style.display = "block";
+                }
+            });
+        }
     }
 }
