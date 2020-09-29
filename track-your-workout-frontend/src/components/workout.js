5075c9c98456bcd18cbc74b4000eb4a145e97a3f
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-29 11:59:39 -0400

    no changes

--- track-your-workout-frontend/src/components/workout.js
+++ track-your-workout-frontend/src/components/workout.js
@@ -8,7 +8,7 @@ class Workout{
 
     renderHTML(){
         return `
-            <div class="${this.id} content"data-set-id="${this.id}"><span class="close">x</span>
+            <div class="${this.id} content"data-set-id="${this.id}"><button class="close">x</button><button class="new-edit">edit</button>
                 <p> The user is: ${this.user} </p>
                 <h3> The workout name is: ${this.workout.name}</h3>
                 <iframe width="420" height="315" src="${this.workout.url}/embed" frameborder="0" allowfullscreen></iframe>
