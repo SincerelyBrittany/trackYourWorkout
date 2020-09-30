02746e630c294190a1f9f6dd046f45e7b65c2ec3
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-30 15:33:01 -0400

    adds youtube adapter and gitignore file obtaining the cfile holding api keys

---
+++ track-your-workout-frontend/src/adapters/YoutubeAdapter.js
@@ -0,0 +1,18 @@
+class YoutubeAdapter {
+    constructor(){
+        this.baseURL = 
+        "https://www.googleapis.com/youtube/v3"
+    }
+
+    searchYoutube(params){
+        return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=relevance&q=${params}&type=video&videoEmbeddable=true&videoSyndicated=true&key=${YOUTUBEAPIKEY}`, {
+          })
+          .then(res=> res.json())
+    }
+}
+
+
+
+`https://www.youtube.com/embed/${videoID}`
+
+
