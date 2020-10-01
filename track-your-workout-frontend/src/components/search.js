e9335e7f8c5c1587354162ac0c06327c0a8db92f
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-01 13:25:43 -0400

    successfully makes request to youtube api and renders onto the page

--- track-your-workout-frontend/src/components/search.js
+++ track-your-workout-frontend/src/components/search.js
@@ -1,9 +1,12 @@
 class Search{
     constructor(){
-        this.search = []
+        this.search = {}
+        this.searchArr = []
         this.adapter = new YoutubeAdapter()
         this.initBindingsAndEventListeners()
         this.renderForm()
+        // console.log(this.searchArr)
+        // console.log(this.search)
         // this.fetchAndLoadWorkouts()  
     }
 
@@ -26,7 +29,26 @@ class Search{
     searchForWorkout(e){
         e.preventDefault()
         this.querySearch = document.getElementById("query").value
-        this.adapter.searchYoutube(this.querySearch).then(workout => console.log(workout))
+        this.adapter.searchYoutube(this.querySearch).then((videos) => {
+            videos["items"].forEach(video => { 
+                const newSearch = new SearchObj(video)
+                this.search[newSearch.videoID] = newSearch
+                const arr = newSearch
+                this.searchArr.push(arr)
+                const searchString = this.searchArr.map((video) => {
+                    return `<div>${video.videoTitle}</div>
+                    <iframe width="420" height="315" src="https://www.youtube.com/embed/${video.videoID}" frameborder="0" allowfullscreen></iframe>
+                    <button "data-set-id="${video.videoID}"> Select <button>`
+                }).join(' ')
+                this.searchContainerForm.innerHTML = `${searchString}`
+            })
+        })
     }
 
 }
+
+        // videoID: video.id.videoId,
+            // let videoTitle = video.snippet.title,
+            // let description = video.snippet.description,
+            // let image = video.snippet.thumbnails.medium)
+            // debugger
---
+++ track-your-workout-frontend/src/components/searchObj.js
@@ -0,0 +1,18 @@
+class SearchObj{
+    constructor(video){
+         this.videoTitle = video.snippet.title
+         this.videoID = video.id.videoId
+        //  this.description = video.snippet.description
+        //  this.image = video.snippet.thumbnails.medium)
+    }
+
+    renderSearchHTML(){
+        return `
+        <div class="${this.videoID} content"data-set-id="${this.videoID}">
+            <span class="selected"> Selected </span>
+            <iframe width="420" height="315" src="https://www.youtube.com/embed/${this.videoID}" frameborder="0" allowfullscreen></iframe>
+            <p> https://www.youtube.com/embed/${this.videoID}</p>
+        </div>`
+    }
+
+}
