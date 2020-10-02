909315ed1be7898c6ad62c64eccb7a316013ad17
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-01 20:38:12 -0400

    shows workouts and search based off clicking navbar buttons

--- track-your-workout-frontend/src/components/navbar.js
+++ track-your-workout-frontend/src/components/navbar.js
@@ -1,32 +1,55 @@
 class NavBar{
     constructor(){
+        this.removeLogin()
         this.renderNavBar()
+    }
+
+    removeLogin(){
+        const login = document.getElementById("login")
+        if(login){
+            login.style.display = "none"
+            login.remove()
+        }
     }
 
     renderNavBar(){
         let nav = document.querySelector("nav")
         nav.innerHTML = `
                 <button id="workout-btn"> workouts </button> 
-                <button id="search-btn"> search </button>`
+                <button id="search-btn"> search </button>
+                <button id="logout-btn"> logout </button>`
         const workBtn = document.getElementById("workout-btn")
         const searchBtn = document.getElementById("search-btn")
+        // const logoutBtn = document.getElementById("logout-btn")
         workBtn.addEventListener("click", () =>{
-            const login = document.getElementById("login")
-            if(login){
-                login.remove()
-            }
+            const searchFormContainerForm = document.getElementById('search-form-container')
+            const searchContainerForm = document.getElementById('search-container')
+            searchFormContainerForm.innerHTML = "" 
+            searchContainerForm.innerHTML = ""
             new Workouts()
         })
 
         searchBtn.addEventListener("click", () =>{
-            const login = document.getElementById("login")
-            if(login){
-                login.remove()
-            }
             const container = document.querySelector(".container")
             container.style.display = "block"
+            const workoutContainerForm = document.getElementById('new-workout-container')
+            const workoutContainer = document.getElementById('workout-container')
+            workoutContainer.innerHTML= ""
+            workoutContainerForm.innerHTML = ""
             new Search()
         })
+        
+        // logoutBtn.addEventListener("click", () =>{
+        //     const searchFormContainerForm = document.getElementById('search-form-container')
+        //     const searchContainerForm = document.getElementById('search-container')
+        //     searchFormContainerForm.innerHTML = "" 
+        //     searchContainerForm.innerHTML = ""
+        //     const workoutContainerForm = document.getElementById('new-workout-container')
+        //     const workoutContainer = document.getElementById('workout-container')
+        //     workoutContainer.innerHTML= ""
+        //     workoutContainerForm.innerHTML = ""
+        //     new Login()
+        // })
     }
 
 
