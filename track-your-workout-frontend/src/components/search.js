94feb5dd3b086609aa48db8d56b58ee873e3fee0
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-01 21:48:35 -0400

    creates a workout upon submit in the createform method

--- track-your-workout-frontend/src/components/search.js
+++ track-your-workout-frontend/src/components/search.js
@@ -6,6 +6,8 @@ class Search{
         this.adapter = new YoutubeAdapter()
         this.initBindingsAndEventListeners()
         this.renderForm()
+        this.workoutsAdapter = new WorkoutsAdapter()
+        // this.workoutsAdapter = new workoutsAdapter()
         // this.openModal = openModal()
         // console.log(this.searchArr)
         // console.log(this.search)
@@ -61,7 +63,6 @@ class Search{
     }
 
     openModal = (id, title) => {
-        debugger
         let modal = document.getElementById("myModal");
         let modalContent = document.querySelector(".modal-content")
         let createform = document.createElement("form")
@@ -72,8 +73,23 @@ class Search{
           name="meeting" value="2020-09-14T00:00" min="2020-09-14T00:00"
           max="2030-06-14T00:00">
         <input type="submit"/>`
-        modalContent.appendChild(updateform)
+        modalContent.appendChild(createform)
         modal.style.display = "block";
+
+        createform.addEventListener("submit", (e)=>{
+            e.preventDefault();
+            this.searchFormContainerForm.innerHTML = ""
+            this.searchContainerForm.innerHTML = ""
+            const name = document.getElementById("workout-name").value
+            const url = document.getElementById("workout-url").value
+            const time = document.getElementById("meeting-time").value
+            const date = document.getElementById("meeting-time").value
+            this.workoutsAdapter.createWorkout(name, url, time, date).then(workout => { 
+                console.log(workout)
+                modal.style.display = "none";
+                new NavBar()
+            })
+        })
  
     }
 
