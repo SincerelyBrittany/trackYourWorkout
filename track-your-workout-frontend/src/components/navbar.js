38b80243ca2da14fc0b54ce138db309fe7504d31
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-01 21:48:03 -0400

    refactors navbar to toggle show search and workouts

--- track-your-workout-frontend/src/components/navbar.js
+++ track-your-workout-frontend/src/components/navbar.js
@@ -2,6 +2,8 @@ class NavBar{
     constructor(){
         this.removeLogin()
         this.renderNavBar()
+        this.initBindingsAndEventListeners()
+        this.toggle()
     }
 
     removeLogin(){
@@ -18,26 +20,49 @@ class NavBar{
                 <button id="workout-btn"> workouts </button> 
                 <button id="search-btn"> search </button>
                 <button id="logout-btn"> logout </button>`
-        const workBtn = document.getElementById("workout-btn")
-        const searchBtn = document.getElementById("search-btn")
-        // const logoutBtn = document.getElementById("logout-btn")
-        workBtn.addEventListener("click", () =>{
-            const searchFormContainerForm = document.getElementById('search-form-container')
-            const searchContainerForm = document.getElementById('search-container')
-            searchFormContainerForm.innerHTML = "" 
-            searchContainerForm.innerHTML = ""
-            new Workouts()
-        })
+    }
 
-        searchBtn.addEventListener("click", () =>{
-            const container = document.querySelector(".container")
-            container.style.display = "block"
-            const workoutContainerForm = document.getElementById('new-workout-container')
-            const workoutContainer = document.getElementById('workout-container')
-            workoutContainer.innerHTML= ""
-            workoutContainerForm.innerHTML = ""
-            new Search()
+    initBindingsAndEventListeners(){
+        this.container = document.querySelector(".container")
+        this.container.style.display = "block"
+        this.workBtn = document.getElementById("workout-btn")
+        this.searchBtn = document.getElementById("search-btn")
+        this.searchFormContainerForm = document.getElementById('search-form-container')
+        this.searchContainerForm = document.getElementById('search-container')
+        this.workoutContainerForm = document.getElementById('new-workout-container')
+        this.workoutContainer = document.getElementById('workout-container')
+    }
+
+    toggle(){
+        if (this.searchContainerForm.children.length === 0 && this.workoutContainerForm.children.length === 0){
+            this.renderSearch()
+            this.renderWorkouts()
+        } else if (this.workoutContainerForm.children.length === 0 && this.searchContainerForm.children.length != 0){
+            this.renderWorkouts()
+        } else (this.workoutContainerForm.children.length != 0 && this.searchContainerForm.children.length != 0)
+            this.renderSearch()
+    }
+       
+        // const logoutBtn = document.getElementById("logout-btn")
+    renderWorkouts = () => {
+        this.workBtn.addEventListener("click", () =>{
+            this.searchFormContainerForm.innerHTML = "" 
+            this.searchContainerForm.innerHTML = ""
+            if (this.workoutContainerForm.children.length === 0){
+                new Workouts()
+            }
         })
+    }
+
+    renderSearch = () => {
+        this.searchBtn.addEventListener("click", () =>{
+            this.workoutContainer.innerHTML= ""
+            this.workoutContainerForm.innerHTML = ""
+            if (this.searchFormContainerForm.children.length === 0){
+                new Search()
+            }
+        })
+    }
         
         // logoutBtn.addEventListener("click", () =>{
         //     const searchFormContainerForm = document.getElementById('search-form-container')
@@ -50,7 +75,7 @@ class NavBar{
         //     workoutContainerForm.innerHTML = ""
         //     new Login()
         // })
-    }
-
+    
+ 
 
 }
