b984ef5bf10967ed3ef2f29421f6c54b4e1f8ac7
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-03 14:01:24 -0400

    removes user name from showing

--- track-your-workout-frontend/src/components/workout.js
+++ track-your-workout-frontend/src/components/workout.js
@@ -9,7 +9,6 @@ class Workout{
     renderHTML(){
         return `
             <div class="${this.id} content"data-set-id="${this.id}"><span class="close">x</span>
-                <p> The user is: ${this.user} </p>
                 <h3> The workout name is: ${this.workout.name}</h3>
                 <iframe width="420" height="315" src="${this.workout.url}/embed" frameborder="0" allowfullscreen></iframe>
                 <p> The time is ${this.workout.time_updated} </p>
