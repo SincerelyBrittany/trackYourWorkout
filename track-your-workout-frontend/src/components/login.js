16f68ac852486f01d814950f8184867533d92b26
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-03 13:58:45 -0400

    adds classname to h1 in login

--- track-your-workout-frontend/src/components/login.js
+++ track-your-workout-frontend/src/components/login.js
@@ -9,9 +9,11 @@ class Login {
         container.style.display = "none"
         const loginContainer = document.getElementById("login")
         const h1 = document.createElement("h1")
+        h1.className = "loginh1"
         h1.innerText = "What is your name?"
         const form = document.createElement("form")
         const input = document.createElement("input")
+        input.id = "username"
         input.name = "username"
         form.appendChild(input)
         form.addEventListener("submit", this.submit)
