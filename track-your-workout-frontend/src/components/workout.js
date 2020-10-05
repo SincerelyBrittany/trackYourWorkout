5a0ff286c0ceb3e15aa585c4c7031ff2d857b72f
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-05 15:58:10 -0400

    removes workout name and time from renderHTML

--- track-your-workout-frontend/src/components/workout.js
+++ track-your-workout-frontend/src/components/workout.js
@@ -9,9 +9,9 @@ class Workout{
     renderHTML(){
         return `
             <div class="${this.id} content"data-set-id="${this.id}"><span class="close">x</span>
-                <h3> The workout name is: ${this.workout.name}</h3>
+                <h3> ${this.workout.name}</h3>
                 <iframe width="420" height="315" src="${this.workout.url}/embed" frameborder="0" allowfullscreen></iframe>
-                <p> The time is ${this.workout.time_updated} </p>
+                <p> ${this.workout.time_updated} </p>
         </div>`
     }   
 }
